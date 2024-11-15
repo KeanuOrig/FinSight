d8842ca06ed536283486c8c408a0931d156608ce
Keanu <keanu@bpoc.co.jp>
2024-11-15 10:15:08 +0800

    add migration in docker

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ RUN pip install --upgrade pip && pip install -r requirements.txt
 COPY . /app
 
 # Run the Django development server
-CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+CMD ["sh", "-c", "python manage.py migrate && python manage.py runserver 0.0.0.0:8000"]
--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python ./app/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python ./app/manage.py migrate
+python manage.py migrate
